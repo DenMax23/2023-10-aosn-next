e295caa69bd4bd3d657185d6af93774f7a042579
DenMax23 <mazurvv911@gmail.com>
2023-12-05 12:05:14 +0200

    2023.12.05 12-00

--- app/components/header.js
+++ app/components/header.js
@@ -2,6 +2,17 @@ import Image from "next/image"
 import Link from "next/link"
 
 const Header = () => {
+  const titleNav=[
+    "Головна сторінка",
+    "Новини",
+    "Події",
+    "Квартальні комітети та комітети мікрорайонів",
+    "Контакти",
+  ];
+  
+
+
+
   return (
 
     <div className="  flex justify-around text-2xl py-5 border-zinc-300 border-2 rounded-b-lg">
@@ -10,11 +21,17 @@ const Header = () => {
         <p className=" text-center text-xl  pl-3 py-1.5">Асоціація органів <br/> самоорганізації населення <br/> м.Вінниці</p>
         </Link>
         <nav className="flex border-slate-500 border-solid rounded-xl  ">
-          <Link className=" hover:bg-slate-400 rounded-xl p-3 w-auto mx-2" href="/dashboard">Головна сторінка</Link>
+          {titleNav.map((titleNav, i) => (
+        <Link key={i} className=" hover:bg-slate-400 rounded-xl p-3 w-auto mx-2" href="/dashboard">{titleNav}
+        </Link>
+        ))}
+
+
+          {/* <Link className=" hover:bg-slate-400 rounded-xl p-3 w-auto mx-2" href="/dashboard">Головна сторінка</Link>
           <Link className=" hover:bg-slate-400 rounded-xl p-3 w-auto mx-2"  href="/dashboard/news">Новини</Link>
           <Link className=" hover:bg-slate-400 rounded-xl p-3 w-auto mx-2" href="/dashboard/events">Події</Link>
           <Link className=" hover:bg-slate-400 rounded-xl p-3 w-auto mx-2" href="/dashboard/quarterly">Квартальні комітети та комітети мікрорайонів</Link>
-          <Link className=" hover:bg-slate-400 rounded-xl p-3 w-auto mx-2" href="/dashboard/contacts">Контакти</Link>
+          <Link className=" hover:bg-slate-400 rounded-xl p-3 w-auto mx-2" href="/dashboard/contacts">Контакти</Link> */}
         </nav>
     </div>
   
