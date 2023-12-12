6514b0ef96780b683b8c8cef6d38b7d62dbcf383
DenMax23 <mazurvv911@gmail.com>
2023-12-12 11:13:58 +0200

    2023.12.12 11-15

--- app/components/headerStart.js
+++ app/components/headerStart.js
@@ -5,11 +5,11 @@ const HeaderStart = () => {
   return (
     <div className="">
     <div className="  flex justify-around text-2xl py-5">
-      <Link href="/" className=" flex h-24 ">
+      {/* <Link href="/" className=" flex h-24 ">
       
         <Image src="/main-logo.png" width={80} height={60} alt='logo' priority />
         <p className=" text-center text-xl  pl-3 py-1.5">Асоціація органів <br/> самоорганізації населення <br/> м.Вінниці</p>
-        </Link>
+        </Link> */}
         <nav className="nav flex pt-7">
             <div className="w-80 ">
           <Link className="" href="/dashboard">Зайти до сайту</Link>
